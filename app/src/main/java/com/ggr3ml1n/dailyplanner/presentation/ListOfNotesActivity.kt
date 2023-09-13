29847d7092c67b67940da969aa4a9d511ff90070
Dinar Subeev <dinars163@gmail.com>
2023-09-13 10:34:49 +0400

    MainActivity -> ListOfNotesActivity

---
+++ app/src/main/java/com/ggr3ml1n/dailyplanner/presentation/ListOfNotesActivity.kt
@@ -0,0 +1,12 @@
+package com.ggr3ml1n.dailyplanner.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ggr3ml1n.dailyplanner.R
+
+class ListOfNotesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_of_notes)
+    }
+}
