2a64ca44f7e0b0dcf33f10addd6756268a30f150
Dinar Subeev <dinars163@gmail.com>
2023-09-13 11:57:07 +0400

    Созданы модельки в data и domain

--- domain/src/main/java/com/ggr3ml1n/domain/models/Note.kt
+++ domain/src/main/java/com/ggr3ml1n/domain/models/Note.kt
@@ -1,3 +1,11 @@
 package com.ggr3ml1n.domain.models
 
-data class Note()
+import java.sql.Timestamp
+
+data class Note(
+    val id: Int,
+    val dataStart: Timestamp,
+    val dateFinish: Timestamp,
+    val name: String,
+    val description: String,
+)
