7875bf79d3aeb19d298ccd7dea1764f6766bb185
Dinar Subeev <dinars163@gmail.com>
2023-11-11 17:34:19 +0400

    Небольшие изменения в скриптах Gradle и DAO

--- data/src/main/java/com/ggr3ml1n/data/db/DAO.kt
+++ data/src/main/java/com/ggr3ml1n/data/db/DAO.kt
@@ -4,20 +4,21 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
+import com.ggr3ml1n.data.storage.NoteData
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface DAO {
 
     @Query("SELECT * FROM notes_table")
-    fun getNotes(): Flow<List<com.ggr3ml1n.data.storage.NoteData>>
+    fun getNotes(): Flow<List<NoteData>>
 
     @Query("DELETE FROM notes_table WHERE id is :id")
     suspend fun deleteNote(id: Int)
 
     @Insert
-    suspend fun insertNote(note: com.ggr3ml1n.data.storage.NoteData)
+    suspend fun insertNote(note: NoteData)
 
     @Update
-    suspend fun updateNote(note: com.ggr3ml1n.data.storage.NoteData)
+    suspend fun updateNote(note: NoteData)
 }
